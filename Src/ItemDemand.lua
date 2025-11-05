e2cc333c3b3ab97c9ef2451e7223cdb88cddf029
BloxGlobe <malebochester@proton.me>
2025-11-06 00:18:31 +0200

    Update.html file Create Website Auto scale file Update Src file and its contents

---
+++ Src/ItemDemand.lua
@@ -0,0 +1,14 @@
+-- OptimisticSide
+
+local ItemDemand = {
+
+	Terrible = 0;
+	Low = 1;
+	Normal = 2;
+	High = 3;
+	Amazing = 4;
+	
+}
+
+
+return ItemDemand
---
+++ Src/ItemTrend.lua
@@ -0,0 +1,14 @@
+-- OptimisticSide
+
+local ItemTrend = {
+
+	Lowering = 0;
+	Unstable = 1;
+	Stable" = 2;
+	Raising = 3;
+	Fluctuating = 4;
+	
+}
+
+
+return ItemTrend
